5b5676daaec2ff81ca5e3d4a8261211692155d34
bhushan <bhushanmandala456@gmail.com>
2025-11-22 23:20:39 +0530

    Fix: Remove swagger-ui-react to resolve Html import error on Render

--- hospital-surgeons/next.config.js
+++ hospital-surgeons/next.config.js
@@ -1,44 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer, webpack }) => {
-    // Aggressively exclude swagger-ui-react from ALL builds (server and client)
-    const problematicPackages = [
-      'swagger-ui-react',
-      'react-immutable-proptypes',
-      'react-immutable-pure-component',
-      'redux-immutable',
-    ];
-
-    // Add to externals for server-side
-    if (isServer) {
-      config.externals = config.externals || [];
-      if (!Array.isArray(config.externals)) {
-        config.externals = [config.externals];
-      }
-      problematicPackages.forEach(pkg => {
-        config.externals.push(pkg);
-      });
-    }
-
-    // Prevent resolution of these packages entirely
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-    };
-    
-    problematicPackages.forEach(pkg => {
-      config.resolve.alias[pkg] = false;
-    });
-
-    // Ignore these packages in module resolution
-    config.plugins = config.plugins || [];
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^(swagger-ui-react|react-immutable-proptypes|react-immutable-pure-component|redux-immutable)$/,
-      })
-    );
-    
-    return config;
-  },
+  // No special webpack config needed - swagger-ui-react has been removed
 };
 
 module.exports = nextConfig;
