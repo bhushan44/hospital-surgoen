0497d5077c61421cf912a993effc8b64afaae118
bhushan <bhushanmandala456@gmail.com>
2025-11-22 22:46:42 +0530

    update package.json and next.config.js to fix build issues

--- hospital-surgeons/next.config.js
+++ hospital-surgeons/next.config.js
@@ -1,15 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, webpack }) => {
     // Exclude swagger-ui-react from server-side bundle to prevent Html import errors
     if (isServer) {
       config.externals = config.externals || [];
       if (Array.isArray(config.externals)) {
-        config.externals.push('swagger-ui-react');
+        config.externals.push({
+          'swagger-ui-react': 'commonjs swagger-ui-react',
+        });
+      } else if (typeof config.externals === 'function') {
+        const originalExternals = config.externals;
+        config.externals = [
+          originalExternals,
+          { 'swagger-ui-react': 'commonjs swagger-ui-react' },
+        ];
       } else {
-        config.externals = [config.externals, 'swagger-ui-react'];
+        config.externals = [
+          config.externals,
+          { 'swagger-ui-react': 'commonjs swagger-ui-react' },
+        ];
       }
     }
+    
+    // Ignore swagger-ui-react during module resolution
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'swagger-ui-react': false,
+    };
+    
     return config;
   },
 };
