7e919868a2ceb52366ffb31a48b777ac0e5b4d65
bhushan <bhushanmandala456@gmail.com>
2025-12-07 11:52:47 +0530

    cleaned repository

--- hospital-surgeons/next.config.js
+++ hospital-surgeons/next.config.js
@@ -1,22 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Exclude design folder from compilation
-  webpack: (config) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/Doctor Dashboard Design/**'],
-    };
-    return config;
-  },
-  // Exclude the old Vite source directory from compilation
-  webpack: (config) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/Hospital Dashboard MVP Design/**'],
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
 
